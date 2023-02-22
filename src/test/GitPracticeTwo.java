21548013c0295867ac846a8e3dc85b2c30b0e13c
isabelb11 <125521793+isabelb11@users.noreply.github.com>
2023-02-21 23:20:09 -0500

    Update GitPracticeTwo.java
    This is for pull practice

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -5,6 +5,7 @@ public class GitPracticeTwo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("This is for push practice");
+      System.out.println("This is for push practice");
 	}
 
 }
